50bd6504cdeeacbaf72e57484a5240c16357e30b
Calypte42 <calypte42@gmail.com>
2018-05-20 01:43:41 +0200

    modif

--- serveur.php
+++ serveur.php
@@ -271,12 +271,12 @@
 	   	$connexion=connexionbd();
 	   	$formule="(6366*acos(cos(radians(".$latitude."))*cos(radians(`latitude`))*cos(radians(`longitude`) -radians(".$longitude."))+sin(radians(".$latitude."))*sin(radians(`latitude`))))";
 
-		$sql="SELECT id, pseudoCommercant, nom,".$formule." AS dist, latitude, longitude FROM Commerce WHERE  ".$formule." <= 10 ORDER BY dist ASC LIMIT 3";
+		$sql="SELECT id, pseudoCommercant, localisation, nom,".$formule." AS dist, latitude, longitude FROM Commerce WHERE  ".$formule." <= 10 ORDER BY dist ASC LIMIT 3";
 
 		$query = $connexion->query($sql);
         $data = null;
 		while ($donnees=$query->fetch()) {
-			$data[]=Array('id'=>$donnees['id'], 'pseudoCommercant'=>$donnees['pseudoCommercant'], 'nom'=>$donnees['nom'],'dist' => $donnees['dist'],'latitude'=>$donnees['latitude'],'longitude'=>$donnees['longitude']);
+			$data[]=Array('id'=>$donnees['id'], 'localisation' =>$donnees['localisation'],'pseudoCommercant'=>$donnees['pseudoCommercant'], 'nom'=>$donnees['nom'],'dist' => $donnees['dist'],'latitude'=>$donnees['latitude'],'longitude'=>$donnees['longitude']);
 		}
 	   	$response = new Response();
 	    $response->setContent(json_encode(utf8ize($data)));
